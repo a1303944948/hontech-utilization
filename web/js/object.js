a32ace99d3317911c7f952c3c7d7039b00f0dcc7
mengge <1303944948@qq.com>
2018-11-14 17:10:51 +0800

    日常推送

---
+++ web/js/object.js
@@ -0,0 +1,10 @@
+var viewObj = [
+	{name:'20吨冲床',jiacon:'50',dongcon:'60'},
+	{name:'35吨冲床',jiacon:'50',dongcon:'60'},
+	{name:'50吨冲床',jiacon:'50',dongcon:'60'},
+	{name:'三星机械臂',jiacon:'50',dongcon:'60'},
+	{name:'五星机械臂',jiacon:'50',dongcon:'60'},
+	{name:'七星机械臂',jiacon:'50',dongcon:'60'},
+	{name:'超级管理设备器',jiacon:'50',dongcon:'60'},
+	{name:'超级设备管理器',jiacon:'50',dongcon:'60'},
+]
