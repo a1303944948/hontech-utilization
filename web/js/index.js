095c350ba7465c66d544d61dc41106f71d8cd785
mengge <1303944948@qq.com>
2018-11-19 15:16:54 +0800

    前端部分完成，等待后端接口

--- web/js/index.js
+++ web/js/index.js
@@ -4,13 +4,31 @@ var app = new Vue({
         products: viewObj,
     },
 })
-for(var i = 0; i < c('index_home_left_item_left').length; i++){
-    console.log(c('index_home_left_item_left')[i].clientHeight);
-    c('index_home_left_item_center')[i].style.height = c('index_home_left_item_left')[i].clientHeight + 1 + 'px';
-    c('index_home_left_item_center')[i].style.lineHeight = c('index_home_left_item_left')[i].clientHeight + 1 + 'px';
-    c('index_home_left_item_center')[i].children[0].style.width = c('index_home_left_item_center')[i].children[0].innerHTML;
-    console.log(c('index_home_left_item_center')[i].children[0].innerHTML);
-    c('index_home_left_item_right')[i].style.height = c('index_home_left_item_left')[i].clientHeight + 1 + 'px';
-    c('index_home_left_item_right')[i].style.lineHeight = c('index_home_left_item_left')[i].clientHeight + 1 + 'px';
-    c('index_home_left_item_right')[i].children[0].style.width = c('index_home_left_item_right')[i].children[0].innerHTML;
+var apps = new Vue({
+    el: '#ranking_home',
+    data: {
+        products: rankObj,
+    }
+})
+var appz = new Vue({
+    el: '#record_home',
+    data: {
+        products: recordObj,
+    }
+})
+
+
+//以下为页面公共方法
+c('footera')[0].ontouchend = function(){location.href = 'index.html'};
+c('footerb')[0].ontouchend = function(){location.href = 'ranking.html'};
+c('footerc')[0].ontouchend = function(){location.href = 'record.html'};
+
+//日期选择默认今天
+var defaultDate = c('index_header_int');
+var date = new Date();
+var count = date.getTime() - 86400000;
+for (var i = 0; i<defaultDate.length; i++) {
+    defaultDate[i].valueAsDate = new Date(count);
 }
+
+//禁用所有页面touchmove事件
