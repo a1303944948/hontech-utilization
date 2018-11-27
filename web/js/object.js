7b7383306f86e4db382627a784734a80048ab2cd
mengge <1303944948@qq.com>
2018-11-27 11:51:05 +0800

    前端页面全部完成，等候后台接口

--- web/js/object.js
+++ web/js/object.js
@@ -29,19 +29,28 @@ var viewObj = [
 	{name:'35吨冲床',jiacon:'40%',dongcon:'90%'},
 ]
 
+//首页稼动率排名  
+//machName设备名称  machId设备编号  owner工厂编号  ownerText工厂名称   text实际稼动率  normal稼动率标准  typeId设备类型编号  typeName设备类型名称
 var rankObj = [
-	{name: '折弯001',machId:'1',owner: 'zjjf',ownerText: '浙江嘉丰',text: '90%',normal: '80%',typeId: 'Bending',typeName: '折弯'},
-	{name: '冲压004',machId:'2',owner: 'zjjf',ownerText: '浙江嘉丰',text: '89%',normal: '80%',typeId: 'Punching',typeName: '冲压'},
-	{name: '手工焊接003',machId:'3',owner: 'dgjf',ownerText: '东莞嘉丰',text: '79%',normal: '80%',typeId: 'welding-robot',typeName: '机器焊接'},
-	{name: '机器焊接002',machId:'4',owner: 'dgjf',ownerText: '东莞嘉丰',text: '78%',normal: '80%',typeId: 'welding',typeName: '手工焊接'},
-	{name: '点焊015',machId:'5',owner: 'zjjf',ownerText: '浙江嘉丰',text: '78%',normal: '80%',typeId: 'spot-welder',typeName: '点焊'},
-	{name: '折弯016',machId:'6',owner: 'dgjf',ownerText: '东莞嘉丰',text: '76%',normal: '70%',typeId: 'Bending',typeName: '折弯'},
-	{name: '折弯017',machId:'7',owner: 'zjjf',ownerText: '浙江嘉丰',text: '70%',normal: '70%',typeId: 'Bending',typeName: '折弯'},
-	{name: '折弯018',machId:'8',owner: 'dgjf',ownerText: '东莞嘉丰',text: '66%',normal: '70%',typeId: 'Bending',typeName: '折弯'},
-	{name: '折弯019',machId:'9',owner: 'dgjf',ownerText: '东莞嘉丰',text: '50%',normal: '80%',typeId: 'Bending',typeName: '折弯'},
+	{machName: '折弯001',machId:'1',owner: 'zjjf',ownerText: '浙江嘉丰',text: '90%',normal: '80%',typeId: 'Bending',typeName: '折弯'},
+	{machName: '冲压004',machId:'2',owner: 'zjjf',ownerText: '浙江嘉丰',text: '89%',normal: '80%',typeId: 'Punching',typeName: '冲压'},
+	{machName: '手工焊接003',machId:'3',owner: 'dgjf',ownerText: '东莞嘉丰',text: '79%',normal: '80%',typeId: 'welding-robot',typeName: '机器焊接'},
+	{machName: '机器焊接002',machId:'4',owner: 'dgjf',ownerText: '东莞嘉丰',text: '78%',normal: '80%',typeId: 'welding',typeName: '手工焊接'},
+	{machName: '点焊015',machId:'5',owner: 'zjjf',ownerText: '浙江嘉丰',text: '78%',normal: '80%',typeId: 'spot-welder',typeName: '点焊'},
+	{machName: '折弯016',machId:'6',owner: 'dgjf',ownerText: '东莞嘉丰',text: '76%',normal: '70%',typeId: 'Bending',typeName: '折弯'},
+	{machName: '折弯017',machId:'7',owner: 'zjjf',ownerText: '浙江嘉丰',text: '70%',normal: '70%',typeId: 'Bending',typeName: '折弯'},
+	{machName: '折弯018',machId:'8',owner: 'dgjf',ownerText: '东莞嘉丰',text: '66%',normal: '70%',typeId: 'Bending',typeName: '折弯'},
+	{machName: '折弯019',machId:'9',owner: 'dgjf',ownerText: '东莞嘉丰',text: '50%',normal: '80%',typeId: 'Bending',typeName: '折弯'},
 ]
-var rankViewObj = {machId: '1',machName: '折床001',group: '邢天铭',manager: '黄勤飞',rank:'1',total: '7200',normal: '8000',text: '90%',arr: [{name: '上午',total: '2160',startTime: '7:40',endTime: '11:50',text: '30%'},{name: '下午',total: '2160',startTime: '13:00',endTime: '17:10',text: '30%'},{name: '晚上',total: '2160',startTime: '17:40',endTime: '20:40',text: '30%'}],time: '2018/11/21',lastSevenDays: [{text: '50%',normal: '80%',date: '11/15',week: '周四'},{text: '80%',normal: '80%',date: '11/16',week: '周五'},{text: '70%',normal: '80%',date: '11/17',week: '周六'},{text: '40%',normal: '80%',date: '11/18',week: '周日'},{text: '20%',normal: '80%',date: '11/19',week: '周一'},{text: '90%',normal: '80%',date: '11/20',week: '周二'},{text: '90%',normal: '80%',date: '11/21',week: '周二'}]};
 
+//首页点击进去后详情页接口
+//machId设备编号   machName设备名称  group组长  manager经理  rank排名  total实际总做功  normal做功标准  text实际稼动率  
+//arr(name时间段名称 total时间段总做功 startTime首次做功时间 endTime末次做功时间 text实际稼动率)上午下午晚上数据集合  time日期  
+//lastSevenDays(text实际做功率 normal标准做功率 date日期 week星期几)最近七天数据集合
+var rankViewObj = {machId: '1',machName: '折床001',group: '邢天铭',manager: '黄勤飞',rank:'1',total: '7200',normal: '8000',text: '90%',arr: [{name: '上午',total: '2160',startTime: '7:40',endTime: '11:50',text: '30%'},{name: '下午',total: '2160',startTime: '13:00',endTime: '17:10',text: '30%'},{name: '晚上',total: '2160',startTime: '17:40',endTime: '20:40',text: '30%'}],time: '2018/11/21',lastSevenDays: [{text: '50%',normal: '80%',date: '11/15',week: '周四'},{text: '80%',normal: '80%',date: '11/16',week: '周五'},{text: '70%',normal: '80%',date: '11/17',week: '周六'},{text: '40%',normal: '80%',date: '11/18',week: '周日'},{text: '20%',normal: '80%',date: '11/19',week: '周一'},{text: '90%',normal: '80%',date: '11/20',week: '周二'},{text: '90%',normal: '80%',date: '11/21',week: '周三'}]};
+
+//记录页面接口
+//machId设备编号 machName设备名称 total今日总计做功 normal标准
 var recordObj = [
 	{machId: '1',machName: '折床001',total: '5600',normal: '8000'+'/8小时'},
 	{machId: '2',machName: '折床002',total: '2300',normal: '8000'+'/8小时'},
@@ -64,15 +73,21 @@ var recordObj = [
 ]
 
 
+//最近三十天页面接口
+//machName设备名称  machId设备ID  owner工厂ID  ownerText工厂名称  text三十天内未做功天数  typeId设备类型编号  typeName设备类型名称
 var last30days = [
-	{name: '折弯001',machId:'1',owner: 'zjjf',ownerText: '浙江嘉丰',text: '12',typeId: 'Bending',typeName: '折弯'},
-	{name: '冲压004',machId:'2',owner: 'zjjf',ownerText: '浙江嘉丰',text: '11',typeId: 'Punching',typeName: '冲压'},
-	{name: '手工焊接003',machId:'3',owner: 'dgjf',ownerText: '东莞嘉丰',text: '9',typeId: 'welding-robot',typeName: '机器焊接'},
-	{name: '机器焊接002',machId:'4',owner: 'dgjf',ownerText: '东莞嘉丰',text: '8',typeId: 'welding',typeName: '手工焊接'},
-	{name: '点焊015',machId:'5',owner: 'zjjf',ownerText: '浙江嘉丰',text: '8',typeId: 'spot-welder',typeName: '点焊'},
-	{name: '折弯016',machId:'6',owner: 'dgjf',ownerText: '东莞嘉丰',text: '6',typeId: 'Bending',typeName: '折弯'},
-	{name: '折弯017',machId:'7',owner: 'zjjf',ownerText: '浙江嘉丰',text: '6',typeId: 'Bending',typeName: '折弯'},
-	{name: '折弯018',machId:'8',owner: 'dgjf',ownerText: '东莞嘉丰',text: '6',typeId: 'Bending',typeName: '折弯'},
-	{name: '折弯019',machId:'9',owner: 'dgjf',ownerText: '东莞嘉丰',text: '5',typeId: 'Bending',typeName: '折弯'},
+	{machName: '折弯001',machId:'1',owner: 'zjjf',ownerText: '浙江嘉丰',text: '12',typeId: 'Bending',typeName: '折弯'},
+	{machName: '冲压004',machId:'2',owner: 'zjjf',ownerText: '浙江嘉丰',text: '11',typeId: 'Punching',typeName: '冲压'},
+	{machName: '手工焊接003',machId:'3',owner: 'dgjf',ownerText: '东莞嘉丰',text: '9',typeId: 'welding-robot',typeName: '机器焊接'},
+	{machName: '机器焊接002',machId:'4',owner: 'dgjf',ownerText: '东莞嘉丰',text: '8',typeId: 'welding',typeName: '手工焊接'},
+	{machName: '点焊015',machId:'5',owner: 'zjjf',ownerText: '浙江嘉丰',text: '8',typeId: 'spot-welder',typeName: '点焊'},
+	{machName: '折弯016',machId:'6',owner: 'dgjf',ownerText: '东莞嘉丰',text: '6',typeId: 'Bending',typeName: '折弯'},
+	{machName: '折弯017',machId:'7',owner: 'zjjf',ownerText: '浙江嘉丰',text: '6',typeId: 'Bending',typeName: '折弯'},
+	{machName: '折弯018',machId:'8',owner: 'dgjf',ownerText: '东莞嘉丰',text: '6',typeId: 'Bending',typeName: '折弯'},
+	{machName: '折弯019',machId:'9',owner: 'dgjf',ownerText: '东莞嘉丰',text: '5',typeId: 'Bending',typeName: '折弯'},
 ]
+
+//最近三十天详情页面接口
+//machId设备ID  machName设备名称  group组长  manager经理  total最近三十天总做功  stop未做功天数  start做功天数  
+//dateArr最近三十天做功明细一共三十条数据，做功传1，未做功传0，时间为当前的前一天为开始时间
 var last30daysView = {machId: '1',machName: '折床001',group: '邢天铭',manager: '黄勤飞',total: '35800',stop: '20',start: '10',dateArr: [1,0,1,1,1,0,1,1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,1,0,0,1,1,1,1,1]};
